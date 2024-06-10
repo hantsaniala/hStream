55b6412262ef62130e44470e6ad0ba147aaf6228
Hantsaniala Eléo <hantsaniala@gmail.com>
2024-06-10 16:42:52 +0300

    Update `serveHlsTs` to avoid 206 status_code

--- pkg/hStream/hStream.go
+++ pkg/hStream/hStream.go
@@ -1,6 +1,7 @@
 package hStream
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
@@ -125,13 +126,21 @@ func serveHlsM3u8(w http.ResponseWriter, r *http.Request, mediaBase string, fold
 	http.ServeFile(w, r, mediaFile)
 }
 
-func serveHlsTs(w http.ResponseWriter, r *http.Request, mediaBase string, folder string, segName string) {
+func serveHlsTs(w http.ResponseWriter, _ *http.Request, mediaBase string, folder string, segName string) {
 	mediaFile := fmt.Sprintf("%s/%s/%s", mediaBase, folder, segName)
+	file, err := os.ReadFile(mediaFile)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+
 	w.Header().Set("Content-Type", "video/MP2T")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
 	w.Header().Set("Content-Type", "application/x-mpegURL")
-	http.ServeFile(w, r, mediaFile)
+	// http.ServeFile(w, r, mediaFile)
+	w.Write(file)
 }
